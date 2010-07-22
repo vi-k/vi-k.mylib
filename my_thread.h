ea352e5cc79bd0c05c712856658005b0165ff7a3
victor dunaev (victor dunaev <victor dunaev (victor.dunaev@gmail.com)>
2010-07-22 17:54:17 +1100

    mylib.h ??? precompiled headers

--- my_thread.h
+++ my_thread.h
@@ -1,13 +1,7 @@
 ﻿#ifndef MY_THREAD_H
 #define MY_THREAD_H
 
-#include <boost/thread/thread.hpp>
-
-#include <boost/thread/mutex.hpp>
-#include <boost/thread/shared_mutex.hpp>
-#include <boost/thread/recursive_mutex.hpp>
-#include <boost/thread/condition_variable.hpp>
-#include <boost/thread/locks.hpp>
+#include <boost/thread.hpp>
 
 using boost::mutex;
 using boost::shared_mutex;
